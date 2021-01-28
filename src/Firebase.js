6e506621571a4bc22a57f141eba5da69389d0939
sumitsapkota0 <sumitsapkota0@gmail.com>
2021-01-28 10:01:23 +0545

    removed hardcoded enviroment varianles

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyD0ktuYjK-uYOackFP5crr8pS5kTT_8hx4",
-    authDomain: "auth-development-c5ac1.firebaseapp.com",
-    projectId: "auth-development-c5ac1",
-    storageBucket: "auth-development-c5ac1.appspot.com",
-    messagingSenderId: "1081678062754",
-    appId: "1:1081678062754:web:b952de9a9866b37f7ea3fb"
+    apiKey: process.env.REACT_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()
